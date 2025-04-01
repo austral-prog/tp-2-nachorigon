7a3cdd4ae48236e89b05a94b20edbadd2407fe91
nachorigon <nacho.rigon05@gmail.com>
2025-04-01 13:20:45 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,10 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    name = "AdA LoVeLAce"
+    print(name.lower())
+    print(name.title())
+    print(name.upper())
+    name_lower = "\t\tada lovelace"
+    print(name_lower.rstrip())
+ada()   
